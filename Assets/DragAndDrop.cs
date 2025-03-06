75bb1e91aa0a26a204d75d3dd31b25fb195df109
anashezam03 <117794635+anashezam03@users.noreply.github.com>
2025-03-06 12:10:17 -0500

    Updated drag and drop for the  red note
    i did remove the button object on the red music note too

---
+++ Assets/DragAndDrop.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("BeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+
+
+ 
+        
+ }
+
